eb1f4b2c9d1e391d0e6eb0526cf19cdd63b75810
Luckylinux <luckyv0545746@gmail.com>
2025-07-28 19:51:06 +0530

    first commit

---
+++ Sorting techniques/insertion_sort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {13, 46, 24, 52, 20, 9, 4, 5, 7, 23, 52};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    for(int i=0; i<n; i++){
+        for(int j=i; j>0; j--){
+            if(arr[j]<arr[j-1]){
+                int temp = arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
+            }
+        }
+    }
+    for(auto c : arr){
+        cout<<c<<endl;
+    }
+
+    return 0;
+}
